c89139b88285015606cbec9e8d2785abe245e246
Moon <munduk.hyun@agoda.com>
2021-01-30 19:53:48 +0700

    Implement code for proxy user config / create user in hdfs

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -31,6 +31,47 @@ function configure() {
     done
 }
 
+function handleUserOptions() {
+    local hadoop_username_prefix="HADOOP_USER_NAME_"
+    local hadoop_user_opt_prefix="HADOOP_USER_OPT"
+    for user in `printenv | grep $hadoop_username_prefix | sed -r "s/^$hadoop_username_prefix.*?=(.+)$/\1/"`; do
+        local optEnv="${hadoop_user_opt_prefix}_${user}"
+        local opt=${!optEnv}
+        if [ "$opt" != "" ]; then
+            local -A paramMap
+            OLD_IFS=$IFS
+            IFS=";"
+            local splitOpt=($opt)
+            
+            for o in ${splitOpt[@]}; do
+                local pair=(`echo "$o" | awk -F"=" '{print $1";"$2}'`)
+                local pairKey="${pair[0]}"
+                local pairValue="${pair[1]}"
+                paramMap["$pairKey"]="$pairValue"
+            done
+            
+            if [ "${paramMap[proxy.user]}" = "true" ]; then
+                local configForHost="hadoop_proxyuser_${user}_hosts"
+                local valueForHost="${paramMap[proxy.hosts]}"
+                
+                if [ "$valueForHost" = "" ]; then
+                    valueForHost="*"
+                fi
+                local configForGroup="hadoop_proxyuser_${user}_groups"
+                local valueForGroup="${paramMap[proxy.groups]}"
+                if [ "$valueForGroup" = "" ]; then
+                    valueForGroup="*"
+                fi
+                
+                echo "Adding proxy user $configForHost=$valueForHost, $configForGroup=$valueForGroup"
+                addProperty /etc/hadoop/core-site.xml "$configForHost" "$valueForHost"
+                addProperty /etc/hadoop/core-site.xml "$configForGroup" "$valueForGroup"
+            fi
+            IFS=$OLD_IFS
+        fi
+    done
+}
+
 PRERUN_PATH=/prerun
 POSTRUN_PATH=/postrun
 
@@ -85,6 +126,9 @@ if [ -n "$GANGLIA_HOST" ]; then
         echo "$module.sink.ganglia.servers=$GANGLIA_HOST:8649"
     done > /etc/hadoop/hadoop-metrics2.properties
 fi
+
+# Add proxy users
+handleUserOptions
 
 function wait_for_it()
 {
--- namenode/initialize.sh
+++ namenode/initialize.sh
@@ -5,3 +5,23 @@ if [ "$SAFE_MODE_STATUS" = "Safe mode is ON" ]; then
     echo "Leaving safemode"
     hdfs dfsadmin -safemode leave
 fi
+
+# create users in hdfs
+# prefix HADOOP_USER_NAME_ will be added in hdfs
+local hadoop_username_prefix="HADOOP_USER_NAME_"
+for user in `printenv | grep $hadoop_username_prefix | sed -r "s/^$hadoop_username_prefix.*?=(.+)$/\1/"`; do
+    # split by comma to check if it's proxy user
+    user_path_in_hdfs="/user/$user"
+    path_exist=`hdfs dfs -ls $user_path_in_hdfs`
+    if [ "$path_exist" = "" ]; then
+        usergroup=`groups $user`
+        if [ "$usergroup" = "" ]; then
+            owner=$user
+        else
+            owner="$user:$usergroup"
+        fi
+        echo "creating user home $user_path_in_hdfs owned by $owner"
+        hdfs dfs -mkdir $user_path_in_hdfs
+        hdfs dfs -chown $owner $user_path_in_hdfs
+    fi 
+done
