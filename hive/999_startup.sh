d61f53609e5bd80a03cfd6582500ee556fa2d93b
Moon <munduk.hyun@agoda.com>
2021-01-31 13:18:55 +0700

    Make hue work

--- hive/999_startup.sh
+++ hive/999_startup.sh
@@ -13,4 +13,4 @@ su --preserve-environment - hive -c "cd $HIVE_HOME/bin && ./hive --service metas
 source /functions.sh
 wait_for_it localhost:9083
 
-su --preserve-environment - hive -c "cd $HIVE_HOME/bin && ./hiveserver2" &
+su --preserve-environment - hive -c "cd $HIVE_HOME/bin && ./hiveserver2 --hiveconf hive.server2.enable.doAs=false" &
