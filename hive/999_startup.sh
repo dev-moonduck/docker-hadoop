1c9e039406ce0d9e20264ce584ddbd5ebedb010b
dev-moonduck <dev.moonduck@gmail.com>
2021-01-31 16:49:36 +0700

    Fix hive starter script to make it work

--- hive/999_startup.sh
+++ hive/999_startup.sh
@@ -8,9 +8,9 @@ hadoop fs -chmod g+w   /user/hive/warehouse
 $HIVE_HOME/bin/schematool -dbType postgres -initSchema
 
 usermod -s /bin/bash hive
-su --preserve-environment - hive -c "cd $HIVE_HOME/bin && ./hive --service metastore" &
+su --preserve-environment hive -c "cd $HIVE_HOME/bin && ./hive --service metastore" &
 
 source /functions.sh
 wait_for_it localhost:9083
 
-su --preserve-environment - hive -c "cd $HIVE_HOME/bin && ./hiveserver2" &
+su --preserve-environment hive -c "cd $HIVE_HOME/bin && ./hiveserver2" &
