0d9f49cec4f09a7d7d88a4e2108fb5ad4f741abf
dev-moonduck <dev.moonduck@gmail.com>
2021-02-06 22:19:59 +0700

    Spark history server, fix wrong spark dir mount

--- hive/999_startup.sh
+++ hive/999_startup.sh
@@ -7,7 +7,6 @@ hadoop fs -chmod g+w   /user/hive/warehouse
 
 $HIVE_HOME/bin/schematool -dbType postgres -initSchema
 
-usermod -s /bin/bash hive
 su --preserve-environment hive -c "cd $HIVE_HOME/bin && ./hive --service metastore" &
 
 source /functions.sh
