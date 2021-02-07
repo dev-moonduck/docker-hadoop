a30a76754dc82abd7d36414f90346f99942c9665
dev-moonduck <dev.moonduck@gmail.com>
2021-02-07 10:33:00 +0700

    Make spark history server work

--- historyserver/002_run_spark_hs.sh
+++ historyserver/002_run_spark_hs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /functions.sh
 wait_for_it localhost:8188
-$JAVA_HOME/bin/java -cp /opt/spark/conf/:/opt/spark/jars/*:/etc/hadoop/:/etc/hadoop:/opt/hadoop-3.3.0/share/hadoop/common/lib/*:/opt/hadoop-3.3.0/share/hadoop/common/*:/opt/hadoop-3.3.0/share/hadoop/hdfs:/opt/hadoop-3.3.0/share/hadoop/hdfs/lib/*:/opt/hadoop-3.3.0/share/hadoop/hdfs/*:/opt/hadoop-3.3.0/share/hadoop/mapreduce/*:/opt/hadoop-3.3.0/share/hadoop/yarn:/opt/hadoop-3.3.0/share/hadoop/yarn/lib/*:/opt/hadoop-3.3.0/share/hadoop/yarn/* -Xmx2g org.apache.spark.deploy.history.HistoryServer
+su --preserve-environment spark -c "$JAVA_HOME/bin/java -cp /opt/spark/conf/:/opt/spark/jars/*:/etc/hadoop/:/opt/hadoop-3.3.0/share/hadoop/common/lib/*:/opt/hadoop-3.3.0/share/hadoop/common/*:/opt/hadoop-3.3.0/share/hadoop/hdfs/:/opt/hadoop-3.3.0/share/hadoop/hdfs/lib/*:/opt/hadoop-3.3.0/share/hadoop/hdfs/*:/opt/hadoop-3.3.0/share/hadoop/mapreduce/*:/opt/hadoop-3.3.0/share/hadoop/yarn/:/opt/hadoop-3.3.0/share/hadoop/yarn/lib/*:/opt/hadoop-3.3.0/share/hadoop/yarn/* -Xmx2g org.apache.spark.deploy.history.HistoryServer --properties-file /spark_history_server.conf"
